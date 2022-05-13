29f899f2f3e1a7c009fbd2cd1464a677840a04f6
jingru <104969882+b-knd@users.noreply.github.com>
2022-05-13 21:50:09 +0800

    Create Q1693LeadsAndPartners.sql

---
+++ SQL/Q1693LeadsAndPartners.sql
@@ -0,0 +1,53 @@
+/*
+1693. Daily Leads and Partners
+
+Question description:
+
+Write an SQL query that will, for each date_id and make_name, return the number of distinct lead_id's and distinct partner_id's.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Input: 
+DailySales table:
++-----------+-----------+---------+------------+
+| date_id   | make_name | lead_id | partner_id |
++-----------+-----------+---------+------------+
+| 2020-12-8 | toyota    | 0       | 1          |
+| 2020-12-8 | toyota    | 1       | 0          |
+| 2020-12-8 | toyota    | 1       | 2          |
+| 2020-12-7 | toyota    | 0       | 2          |
+| 2020-12-7 | toyota    | 0       | 1          |
+| 2020-12-8 | honda     | 1       | 2          |
+| 2020-12-8 | honda     | 2       | 1          |
+| 2020-12-7 | honda     | 0       | 1          |
+| 2020-12-7 | honda     | 1       | 2          |
+| 2020-12-7 | honda     | 2       | 1          |
++-----------+-----------+---------+------------+
+Output: 
++-----------+-----------+--------------+-----------------+
+| date_id   | make_name | unique_leads | unique_partners |
++-----------+-----------+--------------+-----------------+
+| 2020-12-8 | toyota    | 2            | 3               |
+| 2020-12-7 | toyota    | 1            | 2               |
+| 2020-12-8 | honda     | 2            | 2               |
+| 2020-12-7 | honda     | 3            | 2               |
++-----------+-----------+--------------+-----------------+
+Explanation: 
+For 2020-12-8, toyota gets leads = [0, 1] and partners = [0, 1, 2] while honda gets leads = [1, 2] and partners = [1, 2].
+For 2020-12-7, toyota gets leads = [0] and partners = [1, 2] while honda gets leads = [0, 1, 2] and partners = [1, 2].
+*/
+
+
+/*Solution*/
+select distinct date_id, make_name, 
+count(distinct lead_id) as unique_leads, 
+count(distinct partner_id) as unique_partners 
+from DailySales group by date_id,make_name;
+
+/*
+Explanation: 
+Use count to count number of unique lead_id and partner_id together with the keyword DISTINCT
+Use group by to combine results with same date_id and make_name
+*/
