48361cec5e4eec7378dac28e5ab7e90348492c24
jingru <104969882+b-knd@users.noreply.github.com>
2022-05-14 20:38:22 +0800

    Create Q183CustomersWhoNeverOrder.sql

---
+++ SQL/Q183CustomersWhoNeverOrder.sql
@@ -0,0 +1,41 @@
+/*
+183. Customers Who Never Order
+
+Description:
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+*/
+
+select name as Customers from Customers where id not in (select customerId from Orders);
