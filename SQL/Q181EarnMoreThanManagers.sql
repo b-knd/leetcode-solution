6987c180f462f5e6232f40e71236421e0848cbd8
jingru <104969882+b-knd@users.noreply.github.com>
2022-05-14 20:31:06 +0800

    Create Q181EarnMoreThanManagers.sql

---
+++ SQL/Q181EarnMoreThanManagers.sql
@@ -0,0 +1,38 @@
+/*
+181. Employees Earning More Than Their Managers
+
+Description:
+Write an SQL query to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+Explanation: Joe is the only employee who earns more than his manager.
+*/
+
+
+/*Solution*/
+select e.name as Employee from employee e 
+where salary > (select salary from employee m where m.id=e.managerId);
+
