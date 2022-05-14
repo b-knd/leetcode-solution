120c1964f110e9a87fc283df298735bacf68c92a
jingru <104969882+b-knd@users.noreply.github.com>
2022-05-14 20:50:07 +0800

    Create Q197RisingTemperature.sql

---
+++ SQL/Q197RisingTemperature.sql
@@ -0,0 +1,41 @@
+/*
+197. Rising Temperature
+
+Description:
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
+
+/* Solution */
+select id from Weather w where w.temperature > 
+(select temperature from weather w2 where w2.recordDate = w.recordDate - INTERVAL 1 Day);
+
+/* use a nested select to obtain temperature a day before by utilising "INTERVAL 1 Day" */
