fc9bccb77625f6ff95611614b5c6f4d50ca1bf9d
jingru <104969882+b-knd@users.noreply.github.com>
2022-06-08 10:09:34 +0800

    Create Q26RemoveElement.java

---
+++ Java/Q26RemoveElement.java
@@ -0,0 +1,38 @@
+/*
+Description:
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums. 
+More formally, if there are k elements after removing the duplicates, 
+then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. 
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example:
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+public class Q27RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
+//Runtime: 1 ms, faster than 28.02% of Java online submissions for Remove Element.
+//Memory Usage: 41.9 MB, less than 71.04% of Java online submissions for Remove Element.
