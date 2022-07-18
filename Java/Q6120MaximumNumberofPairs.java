40cfbdd8f1a26e1b77f256f36c4d0ba7b827b86a
jingru <104969882+b-knd@users.noreply.github.com>
2022-07-18 08:55:20 +0800

    Create Q6120MaximumNumberofPairs.java

---
+++ Java/Q6120MaximumNumberofPairs.java
@@ -0,0 +1,30 @@
+/*
+@b-knd (jingru) on 18 July 2022
+*/
+
+/*
+Concept:
+- Using hashmap to keep track of each elements total count
+- Loop through hashmap, find how many pair using count / 2, find leftover using count % 2
+*/
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Q6120MaximumNumberofPairsinArray {
+
+    public int[] numberOfPairs(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for(int i: nums){
+            count.put(i, count.getOrDefault(i, 0)+1);
+        }
+        int pairCount = 0;
+        int leftOverCount = 0;
+        for(int i: count.values()){
+
+            pairCount += i / 2;
+            leftOverCount += i % 2;
+        }
+        return new int[] {pairCount, leftOverCount};
+    }
+}
