5b4acb66ff956d50229979d1565600269aa43c4f
jingru <104969882+b-knd@users.noreply.github.com>
2022-07-18 09:39:09 +0800

    Create Q6121QueryKthSmallestTrimeedNumber.java

---
+++ Java/Q6121QueryKthSmallestTrimeedNumber.java
@@ -0,0 +1,32 @@
+/*
+@b-knd (jingru) on 18 July 2022 09:29:00
+Referred to solution by arignote (https://leetcode.cn/u/arignote/)
+*/
+
+/*
+Concept:
+- Loop through queries, create an array of indexes
+- Sort the array by comparing substring (after trimming strings to last kth index)
+- Access first ith index from the sorted array and add to result array
+*/
+
+import java.util.Arrays;
+
+public class Q6121QueryKthSmallestTrimeedNumber {
+    
+    public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
+        int[] result = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            Integer[] index = new Integer[queries[i].length];
+            for(int j = 0; j < queries[i].length; j++){
+                index[j] = j;
+            }
+            int k = nums[0].length() - queries[i][0];
+            
+            //sorting array by comparing substring
+            Arrays.sort(index, (x, y) -> nums[x].substring(k).compareTo(nums[y].substring(k)));
+            result[i] = index[queries[i][0]-1];
+        }
+        return result;
+    }
+}
