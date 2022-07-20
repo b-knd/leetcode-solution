e4c6e9119d64869b876c65a70c0fa3d2a88e2835
jingru <104969882+b-knd@users.noreply.github.com>
2022-07-20 09:37:14 +0800

    Create Q242ValidAnagram.java

---
+++ Java/Q242ValidAnagram.java
@@ -0,0 +1,16 @@
+/*
+@b-knd (jingru) on 20 July 2022 09:36:00
+*/
+
+import java.util.*;
+
+//Create two arrays for s and t, sort the array and compare the two strings formed using String.join()
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        String[] s2 = s.split("");
+        String[] t2 = t.split("");
+        Arrays.sort(s2);
+        Arrays.sort(t2);
+        return String.join("", s2).equals(String.join("", t2));
+    }
+}
