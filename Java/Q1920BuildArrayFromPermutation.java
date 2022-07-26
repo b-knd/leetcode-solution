3ebf34ab71952bbd371f9373ce8b285077b382a0
jingru <104969882+b-knd@users.noreply.github.com>
2022-07-26 11:04:36 +0800

    Create Q1920BuildArrayFromPermutation.java

---
+++ Java/Q1920BuildArrayFromPermutation.java
@@ -0,0 +1,18 @@
+/*
+@b-knd (jingru) on 26 July 2022 11:04:00
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] res = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            res[i] = nums[nums[i]];
+        }
+        return res;
+    }
+}
+
+//Runtime: 1 ms, faster than 99.95% of Java online submissions for Build Array from Permutation.
+//Memory Usage: 54 MB, less than 27.19% of Java online submissions for Build Array from Permutation.
