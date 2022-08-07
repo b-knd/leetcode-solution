6a9d7671b01299a1184d72b59721520d1ac4a53c
jingru <104969882+b-knd@users.noreply.github.com>
2022-08-07 15:44:50 +0800

    Update Q21MergeTwoSortedList.java

--- Java/Q21MergeTwoSortedList.java
+++ Java/Q21MergeTwoSortedList.java
@@ -29,13 +29,12 @@ class Solution {
             if(list1 != null && list2 != null){
                 if(list1.val <= list2.val){
                     curr.next = list1;
-                    curr = curr.next;
                     list1 = list1.next;
                 } else{
                     curr.next = list2;
-                    curr = curr.next;
                     list2 = list2.next;
                 }
+                curr = curr.next;
             } else if(list1 == null){
                 curr.next = list2;
                 break;
