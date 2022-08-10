6c1e018286324fad50075521b849b82bfa536245
jingru <104969882+b-knd@users.noreply.github.com>
2022-08-10 10:21:32 +0800

    Update Q226InvertBinaryTree.java

--- Java/Q226InvertBinaryTree.java
+++ Java/Q226InvertBinaryTree.java
@@ -3,23 +3,22 @@
 */
 
 class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        invertTreeRecurse(root);
-        return root;
-    }
     
-    public TreeNode invertTreeRecurse(TreeNode root){
+    public TreeNode invertTree(TreeNode root){
         if(root != null){
             if(root.left == null && root.right == null){
                 return root;
             }
             TreeNode temp = root.left;
-            root.left = invertTreeRecurse(root.right);
-            root.right = invertTreeRecurse(temp);
+            root.left = invertTree(root.right);
+            root.right = invertTree(temp);
         }
         return root;
     }
 }
+
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
+//Memory Usage: 42.3 MB, less than 10.76% of Java online submissions for Invert Binary Tree.
 
 //Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
 //Memory Usage: 42.1 MB, less than 20.66% of Java online submissions for Invert Binary Tree.
