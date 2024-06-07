dab18be0179ba487ec0e1aa0c239b40d9108313c
jingru <104969882+b-knd@users.noreply.github.com>
2024-06-07 15:39:14 +0800

    Update Q182DuplicateEmails.sql

--- SQL/Q182DuplicateEmails.sql
+++ SQL/Q182DuplicateEmails.sql
@@ -1,39 +1,4 @@
-/*
-182. Duplicate Emails
-
-Description: 
-Write an SQL query to report all the duplicate emails.
-
-Return the result table in any order.
-
-The query result format is in the following example.
-
- 
-
-Example 1:
-
-Input: 
-Person table:
-+----+---------+
-| id | email   |
-+----+---------+
-| 1  | a@b.com |
-| 2  | c@d.com |
-| 3  | a@b.com |
-+----+---------+
-Output: 
-+---------+
-| Email   |
-+---------+
-| a@b.com |
-+---------+
-Explanation: a@b.com is repeated two times.
-*/
-
-/* Solution */
-select email as Email from Person group by email having count(*) > 1;
-
-/* Alternative using "WITH" */
-WITH duplicates as
-(select email, count(*) as count from Person group by email)
-select email from duplicates where count > 1;
+# Write your MySQL query statement below
+SELECT email as Email
+FROM Person GROUP BY email
+HAVING COUNT(email) > 1;
